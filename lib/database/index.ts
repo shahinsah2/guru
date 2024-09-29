9bc15db0d2e86b920563c6473606191508f0cbf2
Naveen DAVULURI <naveen@kanku.in>
2024-09-30 02:11:49 +0530

    updated mongoDB URI

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,32 +4,30 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-interface MongooseCache {
+interface Cached {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
 }
 
-// Use const as cached is never reassigned
-const cached: MongooseCache = (global as typeof global & { mongooseCache?: MongooseCache }).mongooseCache || { conn: null, promise: null };
-
-// Assign cached back to global if it doesn't exist yet
-(global as typeof global & { mongooseCache: MongooseCache }).mongooseCache = cached;
+// Use 'const' for the cached object but modify its internal properties
+const cached: Cached = (global as typeof global & { mongoose: Cached }).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
       dbName: 'guru1',
       bufferCommands: false,
     });
+  }
 
   cached.conn = await cached.promise;
 
   return cached.conn;
 };
 
-
+// Assign cached back to global for subsequent access
+(global as typeof global & { mongoose: Cached }).mongoose = cached;
