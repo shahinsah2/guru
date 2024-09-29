22d6e57a346a3ec645471719e7b9f6f3578e548c
Naveen DAVULURI <naveen@kanku.in>
2024-09-30 00:54:59 +0530

    const updated

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+const cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
