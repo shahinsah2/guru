6a068b75b7ee4b3b235f8ee5d0b887725b341dd9
rajukingR <rajumulinti060@gmail.com>
2024-10-21 19:40:57 +0530

    update

--- app/(admin)/settings/tax_lists/page.jsx
+++ app/(admin)/settings/tax_lists/page.jsx
@@ -1,3 +1,4 @@
+
 import Image from 'next/image';
 import Link from 'next/link';
 import TableSearch from '@/components/TableSearch';
@@ -17,10 +18,6 @@ export default async function TaxListPage() {
   // Fetch the tax list data
   const taxLists = await getAllTaxes();
 
-  // Log the tax lists for debugging
-  console.log('====================================');
-  console.log('Populated Tax Lists:', JSON.stringify(taxLists, null, 2));
-  console.log('====================================');
 
   // Prepare data for the table
   const tableData = taxLists.map(item => ({
@@ -30,7 +27,7 @@ export default async function TaxListPage() {
     active_status: item.active_status ? 'Active' : 'Inactive',
     action: (
       <div className='flex items-center gap-2'>
-        <Link href={`/taxlists/${item._id}`}>
+        <Link href={`/tax_lists/${item._id}`}>
           <button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky'>
             <Image src={'/update.png'} alt='Update' width={16} height={16} />
           </button>
@@ -45,12 +42,12 @@ export default async function TaxListPage() {
   return (
     <div className='bg-white p-4 rounded-md m-4 mt-0 flex-1'>
       {/* Top */}
-      <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-between mb-4'>
         <h1 className='hidden md:block text-lg font-semibold'>All Tax Lists</h1>
         <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
           <TableSearch />
           <div className='flex items-center gap-4 self-end'>
-            <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+            <button className='w-8 h-8 flex items-center justify-center rounded-full bg-gray'>
               <Image src={'/filter.png'} alt='Filter' width={14} height={14} />
             </button>
             <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
