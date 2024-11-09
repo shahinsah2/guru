2a080e2ddc89591047c1582a96ced5888516845b
shahinsah2 <shahinsah@innogenx.in>
2024-11-09 18:32:33 +0530

    IMG

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,45 +4,78 @@ import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
 import { useUser } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { IoEyeOffOutline } from "react-icons/io5";
+import loginImg from '/public/login_img.png' // Ensure this image is in the public folder or update the path
 
 export default function SignInPage() {
   const { isSignedIn, user } = useUser()
   const router = useRouter()
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword)
+  }
 
   useEffect(() => {
-
     if (isSignedIn) {
       router.push("/dashboard")
     }
-
-  },[user, isSignedIn, router])
+  }, [user, isSignedIn, router])
 
   return (
-    <div className="grid w-full flex-grow items-center bg-white px-4 sm:justify-center">
-      <SignIn.Root>
-        <SignIn.Step
-          name="start"
-          className="w-full flex-grow space-y-6 rounded-2xl bg-neutral-900 bg-[radial-gradient(circle_at_50%_0%,theme(colors.white/10%),transparent)] px-4 py-10 ring-1 ring-inset ring-white/5 sm:w-96 sm:px-8 text-white"
-        >
-          <h1 className='text-white text-2xl'>Guru Goutam</h1>
- 
-          <Clerk.GlobalError className="block text-sm text-red-400" />
-          <div className="space-y-2">
-          <Clerk.Field name="identifier">
-            <Clerk.Label>Username {' '}</Clerk.Label>
-            <Clerk.Input type='text' className='text-black' required />
-            <Clerk.FieldError />
-          </Clerk.Field>
-          <Clerk.Field name="password">
-            <Clerk.Label>Password {' '}</Clerk.Label>
-            <Clerk.Input type='password' className='text-black' required />
-            <Clerk.FieldError />
-          </Clerk.Field>
-          <SignIn.Action submit>Sign in</SignIn.Action>
-          </div>  
-        </SignIn.Step>
-      </SignIn.Root>
+    <div className="p-4 grid w-full flex-grow items-center bg-white px-4 sm:justify-center">
+      <div className="row">
+        <div className="col">
+          {/* <img src={loginImg.src} alt="SideImage" className="w-full" /> */}
+        </div>
+        <div className="col middleAll">
+          <SignIn.Root>
+            <SignIn.Step
+              name="start"
+              className="w-full flex-grow space-y-6 rounded-2xl bg-neutral-900 bg-[radial-gradient(circle_at_50%_0%,theme(colors.white/10%),transparent)] px-4 py-10 ring-1 ring-inset ring-white/5 sm:w-96 sm:px-8 text-white cardContainer"
+            >
+              <h1 className='text-white text-2xl'>Guru Goutam</h1>
+              <Clerk.GlobalError className="block text-sm text-red-400" />
+
+              <div className="mb-3">
+                <Clerk.Field name="identifier">
+                  <Clerk.Label>Username* {' '}</Clerk.Label>
+                  <Clerk.Input type='text' className='form-control text-black' placeholder="Enter Username" required />
+                  <Clerk.FieldError />
+                </Clerk.Field>
+              </div>
+
+              <div className="mb-3 position-relative">
+                <Clerk.Field name="password">
+                  <Clerk.Label>Password* </Clerk.Label>
+                  <Clerk.Input type={showPassword ? 'text' : 'password'} className='form-control text-black' placeholder="Enter Password" required />
+                  <button type="button" className="btn-eye mt-3" onClick={handlePasswordToggle}>
+                    {showPassword ? <IoEyeOffOutline /> : <FaEye />}
+                  </button>
+                  <Clerk.FieldError />
+                </Clerk.Field>
+              </div>
+
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="rememberMe" />
+                <label className="form-check-label" htmlFor="rememberMe">
+                  Remember Me
+                </label>
+              </div>
+
+              <SignIn.Action submit className="btn btn-primary">Sign in</SignIn.Action>
+
+              <div className="formFooter mt-4">
+                <p>Forgot Your Password?{" "}
+                  <span><a href="/forgot-password" className="text-blue-500">Click Here</a></span>
+                </p>
+              </div>
+            </SignIn.Step>
+          </SignIn.Root>
+        </div>
+      </div>
     </div>
   )
 }
